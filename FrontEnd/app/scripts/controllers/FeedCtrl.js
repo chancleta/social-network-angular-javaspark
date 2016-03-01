47ad07002d375a1eee34eea7d743ee0929a400b4
unknown <lupena@SF2612.domain.synergiesservices.com>
2016-03-01 17:46:28 -0400

    adding "drag to refresh functionality" only visual

--- FrontEnd/app/scripts/controllers/FeedCtrl.js
+++ FrontEnd/app/scripts/controllers/FeedCtrl.js
@@ -13,5 +13,58 @@ socialNetworkApp.controller('FeedCtrl',["$scope","$location","AuthenticationServ
     $scope.$on("ngRepeatDone",function(){
       FeedService.lazyLoadPosts( $scope.feed );
 
+      var postContainer = document.getElementById("feedcontainer");
+
+      var loader = document.getElementById("touchloader");
+      var isTouched = false;
+      var isMoved = false;
+      var prevY = 0;
+      var movedY =  false;
+      var draggedPixels = 0;
+
+
+      //Add the start of the touching
+      postContainer.addEventListener("touchstart",function(e){
+        isTouched = true;
+        e.preventDefault();
+      },false);
+
+      postContainer.addEventListener("touchend",function(e){
+
+        if(isMoved){
+          //loader.style.display = "none";
+          // loader.style.height = "0px";
+          draggedPixels = 0;
+
+          //Here we need to do a request and reload all the posts
+          loader.style.transition = "max-height 1s ease-in";
+          loader.style.maxHeight = "0px";
+        }
+        isTouched = false;
+        isMoved = false;
+
+        e.preventDefault();
+      },false);
+
+
+      postContainer.addEventListener("touchmove",function(e){
+        if(isTouched){
+          movedY = e.changedTouches[0].clientY > prevY;
+          prevY =  e.changedTouches[0].clientY;
+          if(movedY){
+            loader.style.display = "block";
+            draggedPixels += 1;
+            loader.style.height = draggedPixels+'px';
+            loader.style.transition = "max-height 0.15s ease-out";
+            loader.style.maxHeight = "80px";
+            isMoved = true;
+
+          }
+        }
+        console.log(e.changedTouches[0]);
+        e.preventDefault();
+      },false);
+
+
     });
 }]);
